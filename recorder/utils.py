c6d630e2d19b6f3c0d253cb2ce130fa7cec8b2af
Yuxuan Zuo <yuxuan.zuo@outlook.com>
2020-02-11 07:30:58 +0800

    add log save switch

--- recorder/utils.py
+++ recorder/utils.py
@@ -1,23 +1,32 @@
+# -*- coding: utf-8 -*-
+
 import os
 import logging
 import time
+import json
 
+
+def load_config():
+    with open('config.json', 'r') as f:
+        dict = json.load(f)
+        return dict
 
 def get_logger(name):
-    is_exists = os.path.exists('log')
-    if is_exists is not True:
-        os.mkdir('log')
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     if not logger.handlers:
-        date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-        file_handler = logging.FileHandler(f"log/log-{date}.log")
-        file_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter("%(asctime)s[%(levelname)s]: %(filename)s[line:%(lineno)d] - %(name)s : %(message)s")
         stream_handler = logging.StreamHandler()
         stream_handler.setLevel(logging.INFO)
-        formatter = logging.Formatter("%(asctime)s[%(levelname)s]: %(filename)s[line:%(lineno)d] - %(name)s : %(message)s")
         stream_handler.setFormatter(formatter)
-        file_handler.setFormatter(formatter)
         logger.addHandler(stream_handler)
-        logger.addHandler(file_handler)
+        config = load_config()
+        if config['save_log']:
+            if os.path.exists('log') is not True:
+                os.mkdir('log')
+            date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+            file_handler = logging.FileHandler(f"log/log-{date}.log")
+            file_handler.setLevel(logging.DEBUG)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
     return logger
